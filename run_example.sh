e69634084d1aa9aa3a660b76c906a65e79be47af
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-03-04 13:24:44 +0100

    add classifications to the example

--- run_example.sh
+++ run_example.sh
@@ -1,6 +1,9 @@
 mkdir -p /tmp/iofs/{logs,mounted,real}
 
+# Comment out the classifcations if not generated
+
 ./build/src/iofs \
+  --classificationfile=./blackheap_model.csv \
   --in-db=mydb \
   --in-password=influxdblongpassword123 \
   --in-server=http://localhost:8086 \
